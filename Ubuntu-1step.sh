b2d92ad9c42381e7c375df1a347d748fc9023ddb
f1vestars <eramer@mail.ru>
2025-08-23 21:07:37 +0300

    Update Ubuntu-1step.sh

--- Ubuntu-1step.sh
+++ Ubuntu-1step.sh
@@ -86,7 +86,7 @@ ufw default allow outgoing
 ufw default deny routed
 
 # SSH: limit на выбранном порту
-ufw limit "${SSH_PORT}/tcp" comment "SSH custom port"
+ufw limit "${SSH_PORT}/tcp" comment "SSH custom port (IPv6)" || true
 
 # админский 2222 только с белого IP (без limit)
 if [[ "$ADD_ADMIN" == "yes" ]]; then
